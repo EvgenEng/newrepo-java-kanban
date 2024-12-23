3a0395a2bf01d04437c8c0645182920c1ffdf3e3
EvgenEng <zhenya-chaplygin@yandex.ru>
2024-12-24 01:09:39 +0300

    tests  в папке

---
+++ test/ManagersTests.java
@@ -0,0 +1,25 @@
+package tests;
+
+import manager.Managers;
+import interfaces.HistoryManager;
+import interfaces.TaskManager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagersTests {
+
+    @Test
+
+    void testGetDefault() {
+        TaskManager taskManager = Managers.getDefault();
+        assertNotNull(taskManager, "TaskManager should not be null");
+    }
+
+    @Test
+
+    void testGetDefaultHistory() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
+        assertNotNull(historyManager, "HistoryManager should not be null");
+    }
+}
